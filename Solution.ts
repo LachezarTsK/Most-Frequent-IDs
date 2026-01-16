8ed452f84092edae800edb54f7f16e829cb67973
Lachezar Kutsarov <lkutsarov@gmail.com>
2026-01-16 19:16:38 +0100

    Create Solution.ts

---
+++ Solution.ts
@@ -0,0 +1,37 @@
+
+// const {PriorityQueue} = require('@datastructures-js/priority-queue');
+/*
+ PriorityQueue is internally included in the solution file on leetcode.
+ When running the code on leetcode it should stay commented out. 
+ It is mentioned here just for information about the external library 
+ that is applied for this data structure.
+ */
+
+function mostFrequentIDs(IDs: number[], changeInFrequency: number[]): number[] {
+    const maxID = Math.max(...IDs);
+    const frequencyIDs: number[] = new Array(maxID + 1).fill(0);
+    const resultMostFrequentIDsPerTimeStamp: number[] = new Array(IDs.length).fill(0);
+    const maxHeap = new PriorityQueue<Component>((x, y) => y.frequency - x.frequency);
+
+    for (let i = 0; i < IDs.length; ++i) {
+
+        frequencyIDs[IDs[i]] += changeInFrequency[i];
+        if (frequencyIDs[IDs[i]] > 0) {
+            maxHeap.enqueue(new Component(IDs[i], frequencyIDs[IDs[i]]));
+        }
+
+        while (!maxHeap.isEmpty() && frequencyIDs[maxHeap.front().ID] !== maxHeap.front().frequency) {
+            maxHeap.dequeue();
+        }
+
+        if (!maxHeap.isEmpty()) {
+            resultMostFrequentIDsPerTimeStamp[i] = maxHeap.front().frequency;
+        }
+    }
+
+    return resultMostFrequentIDsPerTimeStamp;
+};
+
+class Component {
+    constructor(public ID: number, public frequency: number) {}
+}
